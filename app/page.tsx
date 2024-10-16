eb22efdabe07b8c7a23dbcdc913325d91d014435
Prem <pprakash312003@gmail.com>
2024-10-17 01:18:11 +0530

    10

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,24 @@
 import axios from "axios";
 
-async function getUserData(){
-
-  // accesing their own backend
-  const response = await axios.get("http://localhost:3000/api/user")
-  
-    return response.data;
-  
+async function getUserDetails() {
+  const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+	return response.data;
 }
 
-//async componets
-export default async function Home(){
-  const userDetails = await getUserData();
+export default async function Home() {
+  const userData = await getUserDetails();
 
   return (
-    <div>
-      hi there
-      {userDetails.email}
-      {userDetails.name}
+    <div className="flex flex-col justify-center h-screen">
+        <div className="flex justify-center">
+            <div className="border p-8 rounded">
+                <div>
+                    Name: {userData?.name}
+                </div>
+                
+                {userData?.email}
+            </div>
+        </div>
     </div>
   );
 }
